01f110ef42884a6fc665bb8d4f27a0a3c0158b15
Sudheej Sudhakaran <sudheej@live.com>
2019-09-18 21:46:31 -0400

    Update uid_entrypoint.sh

--- uid_entrypoint.sh
+++ uid_entrypoint.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-
-if ! whoami &> /dev/null; then
-  if [ -w /etc/passwd ]; then
-    echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
-  fi
-fi
 while true; do sleep 15 ; echo "background"; done &
