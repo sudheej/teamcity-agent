539cf29068005aa15df88de2e6dd51ba903fc4e1
Sudheej Sudhakaran <sudheej@live.com>
2019-09-29 20:55:22 -0400

    add default user in passwd

--- uid_entrypoint.sh
+++ uid_entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# Set current user in nss_wrapper
+USER_ID=$(id -u)
+GROUP_ID=$(id -g)
+
+if [ x"$USER_ID" != x"0" ]; then
+
+    echo "default:x:${USER_ID}:${GROUP_ID}:Default Application User:${HOME}:/sbin/nologin" >> /etc/passwd
+
+fi
 echo "Test this thing...."
 /tmp/teamcity/bin/agent.sh start
 while :; do
