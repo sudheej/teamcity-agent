ef5a8ad6d20e5f58cb31da037cf408066c088b2b
Sudheej Sudhakaran <sudheej@live.com>
2019-09-30 21:54:54 -0400

    added command to add to known host

--- uid_entrypoint.sh
+++ uid_entrypoint.sh
@@ -8,7 +8,8 @@ if [ x"$USER_ID" != x"0" ]; then
     echo "default:x:${USER_ID}:${GROUP_ID}:Default Application User:${HOME}:/sbin/nologin" >> /etc/passwd
 
 fi
-echo "Test this thing...."
+echo "Hello Teamcity...."
+ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 /tmp/teamcity/bin/agent.sh start
 while :; do
 sleep 300
